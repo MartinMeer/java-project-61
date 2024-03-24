fec38d35488a7595690beeb313f08f5905d80800
OlegMartynov <olegmms@gmail.com>
2024-03-24 15:55:50 +0300

    feat: The greeting funcional has been realized in class Cli. Added new Makefile

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,4 +1,14 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Cli {
+    private static String userName;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void greeting () {
+        System.out.print("May I have your name? ");
+        userName = scanner.nextLine();
+        System.out.println("Hello, " +userName+ "!");
+    }
 }
