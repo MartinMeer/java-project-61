65339e5a06ea53251162db48e938b4addfee5269
OlegMartynov <olegmms@gmail.com>
2024-04-20 19:51:09 +0300

    fix(Prime): missed char

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -6,7 +6,7 @@ import org.apache.commons.math3.primes.Primes;
 public class Prime implements Game {
     @Override
     public String getRules() {
-        return "Answer 'yes' if given number is prime. Otherwise answer 'no'";
+        return "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     }
 
     @Override
