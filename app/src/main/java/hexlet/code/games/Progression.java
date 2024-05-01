e6c9d6d2e13089e22e6b1d628e99a359b68eb5e7
OlegMartynov <olegmms@gmail.com>
2024-05-02 00:25:50 +0300

    feat(Progression): add geerateProgresion method

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -5,17 +5,25 @@ import hexlet.code.Utils;
 public final class Progression implements Game {
     private static final int[] RANDOM_ORIGINS = {1, 5};
     private static final int[] RANDOM_BOUNDS = {9, 11, 30};
+    int initialTerm;
+    int difference;
+    int bound;
     @Override
     public String getRules() {
         return "What number is missing in the progression?";
     }
 
+    /*Для генерации прогрессии лучше сделать отдельный детерминированный метод, не зависящий от случайных чисел.
+    При необходимости такой метод будет легче тестировать.
+    Принимать на вход он должен 3 параметра: первое число, длинну прогрессии и шаг, а возвращать массив.*/
     @Override
     public String[] generateRound() {
-        int initialTerm = Utils.randomizer(RANDOM_ORIGINS[0], RANDOM_BOUNDS[2]);
-        int difference = Utils.randomizer(RANDOM_ORIGINS[0], RANDOM_BOUNDS[0]);
-        int bound = Utils.randomizer(RANDOM_ORIGINS[1], RANDOM_BOUNDS[1]);
+        initialTerm = Utils.randomizer(RANDOM_ORIGINS[0], RANDOM_BOUNDS[2]);
+        difference = Utils.randomizer(RANDOM_ORIGINS[0], RANDOM_BOUNDS[0]);
+        bound = Utils.randomizer(RANDOM_ORIGINS[1], RANDOM_BOUNDS[1]);
+
         String[] progression = new String[bound];
+        String[] progression = generateProgression(initialTerm, difference, bound);
         int currentTerm = initialTerm;
         for (int i = 1; i < bound; i++) {
             progression[0] = String.valueOf(initialTerm);
