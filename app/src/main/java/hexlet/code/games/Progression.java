60d6f5ba563b53d05388105697e58119883a9a06
OlegMartynov <olegmms@gmail.com>
2024-05-09 10:01:28 +0300

    fix(Progression): add separate variables for random origins & bounds

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,6 +4,13 @@ import hexlet.code.Utils;
 
 public final class Progression implements Game {
     private static final int[] RANDOM_ORIGINS = {1, 5};
+    private static final int RANDOM_ORIGIN_FOR_INITIAL_TERM = 1;
+    private static final int RANDOM_BOUND_FOR_INITIAL_TERM = 30;
+    private static final int RANDOM_ORIGIN_FOR_DIFFERENCE = 1;
+    private static final int RANDOM_BOUND_FOR_DIFFERENCE = 9;
+    private static final int RANDOM_ORIGIN_PROGRESSION_LENGTH = 5;
+    private static final int RANDOM_BOUND_PROGRESSION_LENGTH = 11;
+    private static final int RANDOM_ORIGIN_FOR_ANSWER_INDEX = 1;
     private static final int[] RANDOM_BOUNDS = {9, 11, 30};
 
     @Override
@@ -15,10 +22,10 @@ public final class Progression implements Game {
     При необходимости такой метод будет легче тестировать.
     Принимать на вход он должен 3 параметра: первое число, длинну прогрессии и шаг, а возвращать массив.*/
 
-    private static String[] generateProgression(int initialTerm, int difference, int bound) {
-        String[] progression = new String[bound];
+    private static String[] generateProgression(int initialTerm, int difference, int progressionLength) {
+        String[] progression = new String[progressionLength];
         int currentTerm = initialTerm;
-        for (int i = 1; i < bound; i++) {
+        for (int i = 1; i < progressionLength; i++) {
             progression[0] = String.valueOf(initialTerm);
             currentTerm += difference;
             progression[i] = String.valueOf(currentTerm);
@@ -30,11 +37,11 @@ public final class Progression implements Game {
     A чтоб привести массив к строке можно использовать метод String.join*/
     @Override
     public String[] generateRound() {
-        int initialTerm = Utils.randomizer(RANDOM_ORIGINS[0], RANDOM_BOUNDS[2]);
-        int difference = Utils.randomizer(RANDOM_ORIGINS[0], RANDOM_BOUNDS[0]);
-        int bound = Utils.randomizer(RANDOM_ORIGINS[1], RANDOM_BOUNDS[1]);
-        String[] progression = generateProgression(initialTerm, difference, bound);
-        int indexOfAnswer = Utils.randomizer(RANDOM_ORIGINS[0], bound - 1);
+        int initialTerm = Utils.randomizer(RANDOM_ORIGIN_FOR_INITIAL_TERM, RANDOM_BOUND_FOR_INITIAL_TERM);
+        int difference = Utils.randomizer(RANDOM_ORIGIN_FOR_DIFFERENCE, RANDOM_BOUND_FOR_DIFFERENCE);
+        int progressionLength = Utils.randomizer(RANDOM_ORIGIN_PROGRESSION_LENGTH, RANDOM_BOUND_PROGRESSION_LENGTH);
+        String[] progression = generateProgression(initialTerm, difference, progressionLength);
+        int indexOfAnswer = Utils.randomizer(RANDOM_ORIGIN_FOR_ANSWER_INDEX, progressionLength - 1);
         String answer = progression[indexOfAnswer];
         progression[indexOfAnswer] = "..";
         String task = String.join(" ", progression);
